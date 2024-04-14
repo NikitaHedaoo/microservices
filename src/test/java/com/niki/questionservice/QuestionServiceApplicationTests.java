6879694e5e17176786d2c8593d9a1f586fc4d567
NikitaHedaoo <nh2296@gmail.com>
2024-04-14 23:22:16 +0530

    Initial commit

---
+++ src/test/java/com/niki/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niki.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
